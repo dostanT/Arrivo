b0c377ed77cc2234923047752117a124052ab3c9
dostanT <turlybekdostan@gmail.com>
2026-02-26 18:58:47 +0500

    Initial Commit

---
+++ Arrivo/ArrivoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArrivoApp.swift
+//  Arrivo
+//
+//  Created by Dostan Turlybek on 26.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct ArrivoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
